aae6a21b868a9229d16f98bf4e14d518be13ead6
Trinity Christiana <trinitycterry@gmail.com>
2021-02-13 22:49:57 -0600

    pretend ternary

--- webpack.config.js
+++ webpack.config.js
@@ -1,74 +1,78 @@
 const webpack = require('webpack');
-const HtmlWebPackPlugin = require("html-webpack-plugin");
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-const dotenv = require('dotenv').config({path: __dirname + '/.env'});
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const dotenv = require('dotenv').config({ path: __dirname + '/.env' });
 module.exports = {
   entry: './src/javascripts/main.js',
-  devtool: "eval-source-map",
+  devtool: 'eval-source-map',
   module: {
     rules: [
       {
-        enforce: "pre",
+        enforce: 'pre',
         test: /\.js$/,
         exclude: /node_modules/,
-        loader: "eslint-loader",
-	      options: {
-          formatter: require('eslint/lib/cli-engine/formatters/stylish')
-        }
+        loader: 'eslint-loader',
+        options: {
+          formatter: require('eslint/lib/cli-engine/formatters/stylish'),
+        },
       },
       {
         test: /\.js$/,
         exclude: /node_modules/,
         use: {
-          loader: "babel-loader"
-        }
+          loader: 'babel-loader',
+        },
       },
       {
         test: /\.html$/,
         use: [
           {
-            loader: "html-loader",
-            options: { minimize: true }
-          }
-        ]
+            loader: 'html-loader',
+            options: { minimize: true },
+          },
+        ],
       },
       {
         test: /\.scss$/,
         use: [
           MiniCssExtractPlugin.loader,
-            { loader: 'css-loader', options: { sourceMap: true, importLoaders: 1 } },
-            { loader: 'sass-loader', options: { sourceMap: true } },
+          {
+            loader: 'css-loader',
+            options: { sourceMap: true, importLoaders: 1 },
+          },
+          { loader: 'sass-loader', options: { sourceMap: true } },
         ],
       },
       {
         test: /\.(png|svg|jpg|jpeg|gif)$/,
-        use: ['file-loader']
+        use: ['file-loader'],
       },
       {
         test: /\.(woff|woff2|eot|ttf|otf)$/,
-        use: ['file-loader']
-      }
-    ]
+        use: ['file-loader'],
+      },
+    ],
   },
   plugins: [
     new HtmlWebPackPlugin({
-      template: "./src/index.html",
-      filename: "./index.html"
+      template: './src/index.html',
+      filename: './index.html',
     }),
     new MiniCssExtractPlugin({
-      filename: "[name].css",
-      chunkFilename: "[id].css"
+      filename: '[name].css',
+      chunkFilename: '[id].css',
     }),
     new webpack.ProvidePlugin({
-      $: "jquery",
-      jQuery: "jquery"
+      $: 'jquery',
+      jQuery: 'jquery',
     }),
-    new webpack.DefinePlugin( {
-      'process.env': JSON.stringify(dotenv.parsed)
+    new webpack.DefinePlugin({
+      // Dynamically access local environment variables based on the environment
+      'process.env': JSON.stringify(process.env ? process.env : dotenv.parsed),
     }),
   ],
   output: {
-		path: __dirname + "/build",
-		filename: "bundle.js"
-	}
+    path: __dirname + '/build',
+    filename: 'bundle.js',
+  },
 };
